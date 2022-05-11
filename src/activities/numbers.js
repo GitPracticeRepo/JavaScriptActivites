dbb6192f4fe980208cb3de101274efab25f7c7c0
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-05-11 12:55:37 +0530

    Added activities

---
+++ src/activities/numbers.js
@@ -0,0 +1,18 @@
+// Refer here for style guide
+// https://google.github.io/styleguide/jsguide.html
+//
+
+/*
+* This function will check if the number is prime or not
+* @param {int} number - The positive number
+* @return {bool} whether the number is prime or not
+*/
+function isPrime(number) {
+    for(let index=2; index<number; index++) {
+        const remainder = number%index;
+        if (remainder === 0) {
+            return false;
+        }
+    }
+    return true;
+}
